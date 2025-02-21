b0982a9f35d8a7c818f3df3ddc94260b3bbaeed8
abhi-codes-prog <Alpha.astute@gmail.com>
2025-02-21 21:02:11 +0530

    category ui

--- lib/user/screens/category.dart
+++ lib/user/screens/category.dart
@@ -4,7 +4,6 @@ import 'package:fitness_app/user/screens/sub_category_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
 
-
 class CustomizeInterestsScreen extends StatefulWidget {
   const CustomizeInterestsScreen({super.key});
 
@@ -16,6 +15,7 @@ class CustomizeInterestsScreen extends StatefulWidget {
 class _CustomizeInterestsScreenState extends State<CustomizeInterestsScreen> {
   int _currentIndex = 0; // Track the selected bottom navigation item
   bool isPaymentSuccessful = false; // Variable to hold payment status
+  int _selectedInterestCount = 0; // Track the number of selected interests
 
   // Track selected interests
   final List<Map<String, dynamic>> interests = [
@@ -41,12 +41,10 @@ class _CustomizeInterestsScreenState extends State<CustomizeInterestsScreen> {
     });
     if (isPaymentSuccessful) {
       print("Payment was successful in CustomizeInterestsScreen!");
-      // You can now use isPaymentSuccessful to conditionally show content
     } else {
       print("Payment was not successful or not yet made in CustomizeInterestsScreen.");
     }
   }
-
 
   void _onBottomNavTapped(int index) {
     setState(() {
@@ -87,119 +85,141 @@ class _CustomizeInterestsScreenState extends State<CustomizeInterestsScreen> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: isPaymentSuccessful?  MyPlanScreen()  :  SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.purple, Colors.deepPurple],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                LinearProgressIndicator(
-                  value: 0.5,
-                  backgroundColor: Colors.grey[300],
-                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+      body: isPaymentSuccessful
+          ? MyPlanScreen() // Show MyPlanScreen if payment is successful
+          : SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.purple, Colors.deepPurple],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
                 ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Time to customize your interests',
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      childAspectRatio: .9,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                    ),
-                    itemCount: interests.length,
-                    itemBuilder: (context, index) {
-                      final interest = interests[index];
-                      bool isSelected = interest['selected'];
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      LinearProgressIndicator(
+                        value: 0.5,
+                        backgroundColor: Colors.grey[300],
+                        valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Time to customize your interests',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20),
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: .9,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10,
+                          ),
+                          itemCount: interests.length,
+                          itemBuilder: (context, index) {
+                            final interest = interests[index];
+                            bool isSelected = interest['selected'];
 
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            interest['selected'] = !isSelected;
-                          });
-                        },
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: isSelected
-                                    ? Colors.purple[100]
-                                    : Colors.white,
-                                border: Border.all(
-                                  color: isSelected ? Colors.white : Colors.grey,
-                                  width: 2,
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (isSelected) {
+                                    // Deselect the interest
+                                    interest['selected'] = false;
+                                    _selectedInterestCount--;
+                                  } else {
+                                    // Check if the user can select another interest
+                                    if (_selectedInterestCount < 2) {
+                                      interest['selected'] = true;
+                                      _selectedInterestCount++;
+                                    } else {
+                                      // Show a SnackBar if the user tries to select more than 2 interests
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                          content: Text('You can select a maximum of 2 interests.'),
+                                          duration: Duration(seconds: 2),
+                                        ),
+                                      );
+                                    }
+                                  }
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: isSelected
+                                      ? Colors.purple[200]
+                                      : Colors.purple[50],
+                                  border: Border.all(
+                                    color: isSelected ? Colors.white : Colors.purple[200]!,
+                                    width: 2,
+                                  ),
+                                ),
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      interest['icon'],
+                                      color: isSelected ? Colors.white : Colors.deepPurple,
+                                      size: 35,
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(interest['interest'],
+                                        style: const TextStyle(
+                                            fontSize: 14, color: Colors.deepPurple),
+                                        textAlign: TextAlign.center),
+                                  ],
                                 ),
                               ),
-                              padding: const EdgeInsets.all(20),
-                              child: Icon(
-                                interest['icon'],
-                                color: isSelected ? Colors.white : Colors.grey,
-                                size: 40,
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(interest['interest'],
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.white)),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    List<String> selectedInterests = interests
-                        .where((interest) => interest['selected'])
-                        .map((interest) => interest['interest'] as String)
-                        .toList();
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CategorySelectionScreen(
-                          selectedInterests: selectedInterests,
+                            );
+                          },
                         ),
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 100, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          List<String> selectedInterests = interests
+                              .where((interest) => interest['selected'])
+                              .map((interest) => interest['interest'] as String)
+                              .toList();
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CategorySelectionScreen(
+                                selectedInterests: selectedInterests,
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 100, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text('Continue',
+                            style: TextStyle(color: Colors.purple)),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
                   ),
-                  child: const Text('Continue',
-                      style: TextStyle(color: Colors.purple)),
                 ),
-                const SizedBox(height: 20),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: _onBottomNavTapped,
